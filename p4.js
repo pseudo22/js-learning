9bf4774bb390d464864d0cf8b1d036b52ba85f14
pseudo22 <hv885541@gmail.com>
2024-03-24 20:41:23 +0530

    done

---
+++ p4.js
@@ -0,0 +1,15 @@
+const items = ["1" , "2" , "3"]
+// for (const key in items) {
+//    console.log(items[key]);  
+//     }
+// items.forEach(x => {
+//     console.log(x);
+// });
+// for (const i of items) {
+//     console.log(i);
+// }
+// const a = items.filter( (x) => 
+// {
+//    return x > "1"
+// })
+// console.log(a);
